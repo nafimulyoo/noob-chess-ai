36322937e3762e23991a34da4f4ed26564e5703d
nafimulyoo <rxnafimulyo@gmail.com>
2025-04-22 01:01:22 +0700

    chess UI and random ai

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import ChessInterface from "@/components/chess-interface"
 export default function Home() {
   return (
     <main className="container mx-auto py-6 px-4">
-      <h1 className="text-3xl font-bold mb-6">The Noob Chess AI</h1>
+      <h1 className="text-3xl font-bold mb-2">The Noob Chess AI</h1>
+      <p className="mb-6">
+        A simple chess AI built with Next.js and React. Made by Nafi karena demot ngerjain TA. Source code: <a href="https://github.com/nafimulyoo/noob-chess-ai" className="underline font-bold">GitHub</a>
+      </p>
       <ChessInterface />
     </main>
   )
